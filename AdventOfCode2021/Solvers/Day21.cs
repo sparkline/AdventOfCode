4c012e6c1c6904c0b94f674fc84447e745702dbd
Jeroen Vonk <J.Vonk@sigmax.nl>
2021-12-22 18:41:22 +0100

    Day21 5 seconds for part B

--- AdventOfCode2021/Solvers/Day21.cs
+++ AdventOfCode2021/Solvers/Day21.cs
@@ -5,14 +5,116 @@ namespace AdventOfCode2021.Solvers
     public class Day21 : Solver
     {
         public Day21() : base(2021, 21) { }
+        
+        /*
+         * Given deterministic dice with d rolls per turn
+         * With p players
+         * The score at iteration i is:
+         * d(ipd+pd+0.5(d+1))
+         * 
+         * 
+         * 
+         */
+        
         protected override object PartA(string input)
         {
-            throw new NotImplementedException();
+            var lines = input.SplitOnNewline();
+            int playerOnePosition = int.Parse(lines[0].Split(":")[1]);
+            int playerTwoPosition = int.Parse(lines[1].Split(":")[1]);
+
+            int d = 3;
+            int p = 2;
+            int i = 0;
+            int p1Score = 0;
+            int p2Score = 0;
+            int rolls = 0;
+            while (p1Score < 1000 && p2Score < 1000)
+            {
+                int p1roll = 3 * (i * 6 + 2);
+                rolls += 3;
+                playerOnePosition = (playerOnePosition + p1roll - 1) % 10 + 1;
+                p1Score += playerOnePosition;
+
+                if (p1Score >= 1000)
+                {
+                    break;
+                }
+
+                rolls += 3;
+                int p2roll = 3 * (i * 6 + 5);
+                playerTwoPosition = (playerTwoPosition + p2roll - 1) % 10 + 1;
+                p2Score += playerTwoPosition;
+
+                i++;
+            }
+
+            int loser = Math.Min(p1Score, p2Score);
+            return loser * rolls;
         }
 
         protected override object PartB(string input)
         {
-            throw new NotImplementedException();
+            var lines = input.SplitOnNewline();
+            int playerOnePosition = int.Parse(lines[0].Split(":")[1]);
+            int playerTwoPosition = int.Parse(lines[1].Split(":")[1]);
+
+            Dictionary<int, int> rollFreqs = Enumerable.Range(1, 3)
+                .SelectMany(firstRoll => Enumerable.Range(1, 3), (firstDie, secondDie) => firstDie + secondDie)
+                .SelectMany(firstTwoRolls => Enumerable.Range(1, 3), (firstTwoDie, thirdDie) => firstTwoDie + thirdDie)
+                .GroupBy(totalRoll => totalRoll)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            long win1, win2;
+            win1 = win2 = 0;
+
+            Stack<(int p1Score, int p1Position, long p1Universes, int p2Score, int p2Position, long p2Universes, bool evenTurn)> playsLeft = new Stack<(int p1Score, int p1Position, long p1Universes, int p2Score, int p2Position, long p2Universes, bool evenTurn)>();
+            playsLeft.Push((p1Score: 0, p1Position: playerOnePosition, p1Universes: 1l, p2Score: 0, p2Position: playerTwoPosition, p2Universes: 1l, evenTurn: true));
+
+            Dictionary<(int dieRoll, int position), int> lookUpCache = new Dictionary<(int score, int position), int>();
+
+            while (playsLeft.Count > 0)
+            {
+                var play = playsLeft.Pop();
+                foreach (var roll in rollFreqs)
+                {
+                    int dieValue = roll.Key;
+                    int dieFreq = roll.Value;
+                    int newScore, score, position;
+                    position = play.evenTurn ? play.p1Position : play.p2Position;
+                    if (!lookUpCache.TryGetValue((dieRoll: dieValue, position: position), out int newPosition))
+                    {
+                        newPosition = (position + dieValue - 1) % 10 + 1;
+                        lookUpCache.Add((dieRoll: dieValue, position: position), newPosition);
+                    }
+                    score = play.evenTurn ? play.p1Score : play.p2Score;
+                    newScore = score + newPosition;
+
+                    if (newScore >= 21)
+                    {
+                        win1 += play.evenTurn ? play.p1Universes * dieFreq : 0;
+                        win2 += play.evenTurn ? 0 : play.p2Universes * dieFreq;
+                    } 
+                    else
+                    {
+                        if (play.evenTurn)
+                        {
+                            playsLeft.Push((p1Score: newScore, p1Position: newPosition, p1Universes: play.p1Universes * dieFreq, 
+                                p2Score: play.p2Score, p2Position: play.p2Position, p2Universes: play.p2Universes * dieFreq, 
+                                evenTurn: !play.evenTurn));
+                        } 
+                        else
+                        {
+                            playsLeft.Push((p1Score: play.p1Score, p1Position: play.p1Position, p1Universes: play.p1Universes * dieFreq,
+                                p2Score: newScore, p2Position: newPosition, p2Universes: play.p2Universes * dieFreq,
+                                evenTurn: !play.evenTurn));
+                        }
+
+                    }
+                }
+
+            }
+
+            return Math.Max(win1, win2);
         }
     }
 }
--- UnitTests/AdventOfCode2021Test.cs
+++ UnitTests/AdventOfCode2021Test.cs
@@ -639,7 +639,7 @@ namespace UnitTests
 
             var result = solution.TestPartAResult();
 
-            Assert.AreEqual(null, result);
+            Assert.AreEqual(739785, result);
         }
 
         [TestMethod]
@@ -649,7 +649,7 @@ namespace UnitTests
 
             var result = solution.TestPartBResult();
 
-            Assert.AreEqual(null, result);
+            Assert.AreEqual(444356092776315l, result);
         }
 
         [TestMethod]
